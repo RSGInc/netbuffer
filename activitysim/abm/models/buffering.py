938a6a31a3cd0edbf784de53466ca2cc73a96d05
Stefan Coe <coestefan@gmail.com>
2017-06-23 09:19:33 -0700

    All buffering functionality used by PSRC is now available.

--- activitysim/abm/models/buffering.py
+++ activitysim/abm/models/buffering.py
@@ -31,19 +31,15 @@ def buffer_parcels_settings(configs_dir):
 def buffer_parcels(settings, buffer_parcels_spec, buffer_parcels_settings, parcel_data, data_dir):
 
     """
-    Compute accessibility for each zone in land use file using expressions from accessibility_spec
+    Performs buffering for each point in parcel file using expressions from buffer_parcels_spec
 
-    The actual results depend on the expressions in accessibility_spec, but this is initially
-    intended to permit implementation of the mtc accessibility calculation as implemented by
-    Accessibility.job
+    The actual results depend on the expressions in buffer_parcels_spec, but this is initially
+    intended to replicate PSRC's Soundcast (Daysim) parcel accessibilities. 
 
-    Compute measures of accessibility used by the automobile ownership model.
-    The accessibility measure first multiplies an employment variable by a mode-specific decay
-    function.  The product reflects the difficulty of accessing the activities the farther
-    (in terms of round-trip travel time) the jobs are from the location in question. The products
-    to each destination zone are next summed over each origin zone, and the logarithm of the
-    product mutes large differences.  The decay function on the walk accessibility measure is
-    steeper than automobile or transit.  The minimum accessibility is zero.
+    Using a point file representing a land use boundary (parcel), quanitifes the amount or distance of
+    a varaible associated with the zone. For example, the total number of jobs within a half mile 
+    for each parcel for the distance to the nearest bus stop from each parcel.  
+  
     """
 
     logger.info("Running buffer_parcels")
@@ -69,19 +65,38 @@ def buffer_parcels(settings, buffer_parcels_spec, buffer_parcels_settings, parce
     poi_fname = os.path.join(data_dir, settings["transit_stops"]) 
     poi_df = pd.read_csv(poi_fname, index_col = False)
  
-    
+    # intersections:
+    # combine from and to columns
+    all_nodes = pd.DataFrame(network.edges_df['from'].append(network.edges_df.to), columns = ['node_id'])
 
-    #parcel_data_df['test'] = res.loc[parcel_data_df.node_id].values
+    # get the frequency of each node, which is the number of intersecting ways
+    intersections_df = pd.DataFrame(all_nodes.node_id.value_counts())
+    intersections_df = intersections_df.rename(columns = {'node_id' : 'edge_count'})
+    intersections_df.reset_index(0, inplace = True)
+    intersections_df = intersections_df.rename(columns = {'index' : 'node_id'})
+
+    # add a column for each way count
+    intersections_df['nodes1'] = np.where(intersections_df['edge_count']==1, 1, 0)
+    intersections_df['nodes3'] = np.where(intersections_df['edge_count']==3, 1, 0)
+    intersections_df['nodes4'] = np.where(intersections_df['edge_count']>3, 1, 0)
 
     locals_d = {
         'network': network
     }
+
+    # perhaps these could be stored in locals_d
+    df_dic = {
+        'parcels_df' : parcel_data_df,
+        'intersections_df' : intersections_df,
+        'poi_df': poi_df
+        }
+
     
     if constants is not None:
         locals_d.update(constants)
 
     results, trace_results, trace_assigned_locals \
-        = buffer.buffer_variables(buffer_parcels_spec, parcel_data_df, poi_df, locals_d, trace_rows=None)
+        = buffer.buffer_variables(buffer_parcels_spec, 'parcels_df', df_dic, locals_d, trace_rows=None)
     
 
     tracing.trace_df(results,
@@ -91,73 +106,3 @@ def buffer_parcels(settings, buffer_parcels_spec, buffer_parcels_settings, parce
                              transpose = False)
         
 
-#@orca.injectable()
-#def nearest_poi_spec(configs_dir):
-#    f = os.path.join(configs_dir, 'nearest_poi.csv')
-#    return assign.read_assignment_spec(f)
-
-
-#@orca.step()
-#def distance_to_nearest_poi(settings, nearest_poi_spec, buffer_parcels_settings, parcel_data, data_dir):
-
-#    """
-#    Compute accessibility for each zone in land use file using expressions from accessibility_spec
-
-#    The actual results depend on the expressions in accessibility_spec, but this is initially
-#    intended to permit implementation of the mtc accessibility calculation as implemented by
-#    Accessibility.job
-
-#    Compute measures of accessibility used by the automobile ownership model.
-#    The accessibility measure first multiplies an employment variable by a mode-specific decay
-#    function.  The product reflects the difficulty of accessing the activities the farther
-#    (in terms of round-trip travel time) the jobs are from the location in question. The products
-#    to each destination zone are next summed over each origin zone, and the logarithm of the
-#    product mutes large differences.  The decay function on the walk accessibility measure is
-#    steeper than automobile or transit.  The minimum accessibility is zero.
-#    """
-
-#    logger.info("Running buffer_parcels")
-
-#    constants = config.get_model_constants(buffer_parcels_settings)
-
-#    net_fname = os.path.join(data_dir, settings["buffer_network"])
-#    network = pdna.Network.from_hdf5(net_fname)
-    
-
-#    network.init_pois(num_categories=1, max_dist=2000, max_pois=5)
-
-#    # TO-FIX-reading the following df directly for now since easier table reading is being added.
-#    # this table provides the location of transit stops and types of transit they serve
-#    # used to find distance from parcels to nearest bus, light rail, commuter rail etc.
-#    # also- might be better to store in the master h5 file.  
-#    poi_fname = net_fname = os.path.join(data_dir, settings["transit_stops"]) 
-
-#    parcel_data_df = parcel_data.to_frame()
-#    parcel_data_df.reset_index(level = 0, inplace = True)
- 
-#    # attach the node_id of the nearest network node to each parcel
-#    parcel_data_df['node_id'] = network.get_node_ids(parcel_data_df['xcoord_p'].values, parcel_data_df['ycoord_p'].values)
-
-#    #network.set_pois("bus", poi_df['x'], poi_df['y'])
-#    #res = network.nearest_pois(2000, "bus", num_pois=1, max_distance=99999)
-
-    
-
-#    locals_d = {
-#        'network': network,
-#        'poi_df': pd.read_csv(poi_fname, index_col = False)
-#    }
-    
-#    if constants is not None:
-#        locals_d.update(constants)
-
-#    results, trace_results, trace_assigned_locals \
-#        = assign.assign_variables(nearest_poi_spec, network.nodes_df, locals_d, trace_rows=None)
-    
-#    parcel_data_df['test'] = results.loc[parcel_data_df.node_id].values
-
-#    #tracing.trace_df(results,
-#    #                         label='buffered_parcels',
-#    #                         index_label='None',
-#    #                         slicer='NONE',
-#    #                         transpose = False)
--- activitysim/core/buffer.py
+++ activitysim/core/buffer.py
@@ -46,8 +46,10 @@ def undupe_column_names(df, template="{} ({})"):
 def read_buffer_spec(fname,
                          description_name="Description",
                          target_name="Target",
-                         expression_name="Expression",
-                         variable_name = "Variable"):
+                         variable_name = "Variable",
+                         target_df_name = "TargetDF",
+                         expression_name="Expression"
+                         ):
     """
     Read a CSV model specification into a Pandas DataFrame or Series.
 
@@ -66,13 +68,18 @@ def read_buffer_spec(fname,
         Name of the column in `fname` that contains the component description.
     target_name : str, optional
         Name of the column in `fname` that contains the component target.
+    variable_name : str, optional
+        Name of the column in `fname` that contains the variable target.
+    target_df_name : str, optional
+        Name of the column in `fname` that contains the target dataframe.
     expression_name : str, optional
         Name of the column in `fname` that contains the component expression.
+    
 
     Returns
     -------
     spec : pandas.DataFrame
-        dataframe with three columns: ['description' 'target' 'expression']
+        dataframe with three columns: ['description' 'variable' 'target_df' 'target' 'expression']
     """
 
     cfg = pd.read_csv(fname, comment='#')
@@ -83,7 +90,8 @@ def read_buffer_spec(fname,
     cfg.rename(columns={target_name: 'target',
                         expression_name: 'expression',
                         description_name: 'description',
-                        variable_name: 'variable'},
+                        variable_name: 'variable',
+                        target_df_name: 'target_df'},
                inplace=True)
 
     # backfill description
@@ -107,15 +115,30 @@ class NumpyLogger(object):
         self.logger.error("expression: %s = %s" % (str(self.target), str(self.expression)))
 
 
-def buffer_variables(buffer_expressions, df, poi_df, locals_dict, df_alias=None, trace_rows=None):
+def buffer_variables(buffer_expressions, parcel_df_name, df_dict, locals_dict, df_alias=None, trace_rows=None):
     """
-    Evaluate a set of variable expressions from a spec in the context
-    of a given data table.
+    Perform network buffering on point based data (e.g. parcel centroids) using a 
+    set of variable expressions from a spec in the context of a given data table.
 
     Expressions are evaluated using Python's eval function.
-    Python expressions have access to variables in locals_d (and df being
-    accessible as variable df.) They also have access to previously assigned
+    Python expressions have access to variables in locals_d and df_dict.
+    They also have access to previously assigned
     targets as the assigned target name.
+
+    parcel_df_name is the name of the data frame in df_dict to which all 
+    buffering results will be indexed. The index of this dataframe is specified 
+    in the .yml file. The closest nodes in the pandana network must be set to each 
+    dataframe in df_dict before calling this module. This is achieved using 
+    network.set or network.set_poi. The buffering operations are performed on 
+    each node in the network, thus allowing the results to be joined to each dataframe 
+    via node_id. Only the results that share the same nodes in the parcel_df_name 
+    data frame are returned. The index of this dataframe is specified in .yml file. 
+
+    For example, in order to find the distance of each parcel to the nearest bus stop, 
+    we need a data frame representing bus stop locations and their nearest network node. 
+    Pandana then finds the distance from every node in the network to the nearest node that
+    represents a bus stop. Next, only the distances for nodes that are associated with
+    the parcel dataframe are kept and the results are indexed to the parcel dataframe. 
 
     lowercase variables starting with underscore are temp variables (e.g. _local_var)
     and not returned except in trace_restults
@@ -129,10 +152,18 @@ def buffer_variables(buffer_expressions, df, poi_df, locals_dict, df_alias=None,
 
     Parameters
     ----------
-    assignment_expressions : pandas.DataFrame of target assignment expressions
+    buffer_expressions : pandas.DataFrame of target assignment expressions
         target: target column names
-        expression: pandas or python expression to evaluate
-    df : pandas.DataFrame
+        variable: target variable to be buffered
+        target_df: datafram that contains the variable to be buffered. 
+        expression: pandana, pandas or python expression to evaluate
+    parcel_df_name: the name of the df in df_dict to which all results 
+        will be indexed. 
+    df_dict : dictionary of pandas.DataFrames. This must include the df
+        referenced by parcel_df_name. A poi_df can be used to find distances
+        to other points like bus stops. All poi's should be stored in this 
+        one df. Other dfs can be used for aggregate buffering such as 
+        intersections_df. 
     locals_d : Dict
         This is a dictionary of local variables that will be the environment
         for an evaluation of "python" expression.
@@ -172,18 +203,21 @@ def buffer_variables(buffer_expressions, df, poi_df, locals_dict, df_alias=None,
 
     # avoid touching caller's passed-in locals_d parameter (they may be looping)
     locals_dict = locals_dict.copy() if locals_dict is not None else {}
+    df = df_dict[parcel_df_name]
     if df_alias:
         locals_dict[df_alias] = df
     else:
         locals_dict['df'] = df
     local_keys = locals_dict.keys()
 
+    
+
     l = []
     # need to be able to identify which variables causes an error, which keeps
     # this from being expressed more parsimoniously
-    for e in zip(buffer_expressions.target, buffer_expressions.variable, buffer_expressions.expression):
-        target, var, expression = e
-
+    for e in zip(buffer_expressions.target, buffer_expressions.variable, buffer_expressions.target_df, buffer_expressions.expression):
+        target, var, target_df, expression = e
+        
         if target in local_keys:
             logger.warn("assign_variables target obscures local_d name '%s'" % str(target))
 
@@ -206,22 +240,26 @@ def buffer_variables(buffer_expressions, df, poi_df, locals_dict, df_alias=None,
             network = locals_dict['network']
             # aggregate query
             if 'aggregate' in expression:
-                network.set(df['node_id'], variable=df[var], name=var)
+                network.set(df_dict[target_df]['node_id'], variable=df_dict[target_df][var], name=var)
                 values = to_series(eval(expression, globals(), locals_dict), target=target)
-                df[target] = values.loc[df.node_id].values
-                values = df[target]
-            # nearest poi
+                # index results to the parcel_df:
+                df_dict[parcel_df_name][target] = values.loc[df_dict[parcel_df_name].node_id].values 
+                values = df_dict[parcel_df_name][target] 
+            
+                # nearest poi
             elif 'nearest_pois' in expression:
-                temp_df = poi_df[(poi_df[var] == 1)]
+                temp_df = df_dict['poi_df'][(df_dict['poi_df'][var] == 1)]
                 network.set_pois(var, temp_df['x'], temp_df['y'])
                 values = to_series(eval(expression, globals(), locals_dict), target=target)
-                df[target] = values.loc[df.node_id].values
-                values = df[target]
-            # panda assignment:
+                # index results to the parcel_df:
+                df_dict[parcel_df_name][target] = values.loc[df_dict[parcel_df_name].node_id].values
+                values = df_dict[parcel_df_name][target] 
+            
+            # panda df assignment:
             else:
                 values = to_series(eval(expression, globals(), locals_dict), target=target)
-                # the df might need this column in order to buffer
-                df = df.merge(pd.DataFrame(values, columns=[target]), how = 'left', left_index = True, right_index = True)
+                # the parcel_df might need this column for a subsequent buffer operation
+                df_dict[parcel_df_name] = df_dict[parcel_df_name].merge(pd.DataFrame(values, columns=[target]), how = 'left', left_index = True, right_index = True)
             np.seterr(**save_err)
             np.seterrcall(saved_handler)
 
