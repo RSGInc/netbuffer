db158405df8ddc6b1f5376eb521b22af64827789
bstabler <benstabler@yahoo.com>
2018-03-19 13:05:50 -0700

    continued cleanup

--- example_psrc/extensions/__init__.py
+++ example_psrc/extensions/__init__.py
@@ -1,4 +1,1 @@
-import households
-import persons
-import landuse
 import parcels
--- netbuffer/abm/tables/__init__.py
+++ netbuffer/abm/tables/__init__.py
@@ -1,12 +1,4 @@
 # ActivitySim
 # See full license in LICENSE.txt.
 
-import households
-import persons
-import landuse
-import skims
-import accessibility
-import tours
-import size_terms
-import trips
 import parcels
