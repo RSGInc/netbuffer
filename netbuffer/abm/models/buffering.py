fix off-by-one bug due to parcel index not being saved, switched from transit-stops table to general poi table, cleaned-up example, pycodestyle, added a little bit of documentation

netbuffer/abm/models/buffering.py
@@ -1,3 +1,4 @@
+
 import logging
 import os
 
@@ -16,8 +17,7 @@ logger = logging.getLogger(__name__)
 
 @inject.injectable()
 def buffer_parcels_spec(configs_dir, buffer_parcels_settings):
-    f = os.path.join(
-        configs_dir, buffer_parcels_settings['buffer_parcels_spec'])
+    f = os.path.join(configs_dir, buffer_parcels_settings['buffer_parcels_spec'])
     return buffer.read_buffer_spec(f)
 
 
@@ -32,20 +32,16 @@ def buffer_parcels(settings, buffer_parcels_spec,
                    parcel_data, data_dir, output_dir, trace_parcels):
 
     """
-    Performs network buffering (using Pandana libary
-    http://udst.github.io/pandana/) for each point in parcel
-    file using expressions from buffer_parcels_spec.
+    Performs network buffering (using Pandana libary http://udst.github.io/pandana/)
+    for each point in parcel file using expressions from buffer_parcels_spec.
 
-    The actual results depend on the expressions in
-    buffer_parcels_spec, but this is initially intended to
-    replicate PSRC's Soundcast (Daysim) parcel accessibilities.
+    The actual results depend on the expressions in buffer_parcels_spec, but this is initially
+    intended to replicate PSRC's Soundcast (Daysim) parcel accessibilities.
 
-    Using a point file representing the centroid of a land use
-    boundary (parcel), quanitifes the amount of some variable
-    within a specified distance or the distance to a variable.
-    For example, the total number of jobs within a half mile of
-    each parcel or the distance to the nearest bus stop from each
-    parcel.
+    Using a point file representing the centroid of a land use boundary (parcel), quanitifes
+    the amount of some variable within a specified distance or the distance to a variable. For
+    example, the total number of jobs within a half mile of each parcel or the distance to the
+    nearest bus stop from each parcel.
 
     """
 
@@ -54,62 +50,51 @@ def buffer_parcels(settings, buffer_parcels_spec,
     constants = config.get_model_constants(buffer_parcels_settings)
 
     # create pandana network
-    network_fname = os.path.join(
-        data_dir, buffer_parcels_settings["buffer_network"])
+    network_fname = os.path.join(data_dir, buffer_parcels_settings["buffer_network"])
     network = pdna.Network.from_hdf5(network_fname)
     network.init_pois(num_categories=constants["num_categories"],
-                      max_dist=constants["max_dist"],
-                      max_pois=constants["max_pois"])
+                      max_dist=constants["max_dist"], max_pois=constants["max_pois"])
 
     parcel_data_df = parcel_data.to_frame()
     parcel_data_df.reset_index(level=0, inplace=True)
 
     # attach the node_id of the nearest network node to each parcel
-    parcel_data_df[constants['nodes-id']] = network.get_node_ids(
-        parcel_data_df[constants['parcels-x']].values,
-        parcel_data_df[constants['parcels-y']].values)
-    parcel_data_df.set_index(
-        constants['parcel_index'], inplace=True, drop=False)
+    parcel_data_df[constants['nodes-id']] = \
+        network.get_node_ids(parcel_data_df[constants['parcels-x']].values,
+                             parcel_data_df[constants['parcels-y']].values)
+    parcel_data_df.set_index(constants['parcel_index'], inplace=True, drop=False)
+    inject.add_table('parcel_data', parcel_data_df)
 
     # trace parcels are specified by parcelid
     if 'trace_parcels' in buffer_parcels_settings:
         trace_parcels = buffer_parcels_settings['trace_parcels']
-        trace_parcel_rows = parcel_data_df[
-            constants['parcel_index']].isin(trace_parcels)
+        trace_parcel_rows = parcel_data_df[constants['parcel_index']].isin(trace_parcels)
     else:
         trace_parcel_rows = None
 
-    # point of interest file (e.g. location of bus stops)
-    poi_fname = os.path.join(
-        data_dir, buffer_parcels_settings["transit_stops"])
+    # read pois table
+    poi_fname = os.path.join(data_dir, buffer_parcels_settings["pois"])
     poi_df = pd.read_csv(poi_fname, index_col=False)
-    poi_df[constants['nodes-id']] = network.get_node_ids(
-        poi_df[constants["stops-x"]].values,
-        poi_df[constants["stops-y"]].values)
+    poi_df[constants['nodes-id']] = network.get_node_ids(poi_df[constants["pois-x"]].values,
+                                                         poi_df[constants["pois-y"]].values)
 
     # intersections:
     # combine from and to columns
-    all_nodes = pd.DataFrame(
-        network.edges_df[constants["links-a"]].append(
-            network.edges_df[constants["links-b"]]),
-        columns=[constants['nodes-id']])
+    all_nodes = pd.DataFrame(network.edges_df[constants["links-a"]].append(
+                             network.edges_df[constants["links-b"]]),
+                             columns=[constants['nodes-id']])
 
     # get the frequency of each node, which is the number of intersecting ways
-    intersections_df = pd.DataFrame(
-        all_nodes[constants['nodes-id']].value_counts())
-    intersections_df = intersections_df.rename(
-        columns={constants['nodes-id']: 'edge_count'})
+    intersections_df = pd.DataFrame(all_nodes[constants['nodes-id']].value_counts())
+    intersections_df = intersections_df.rename(columns={constants['nodes-id']: 'edge_count'})
     intersections_df.reset_index(0, inplace=True)
-    intersections_df = intersections_df.rename(
-        columns={'index': constants['nodes-id']})
+    intersections_df = intersections_df.rename(columns={'index': constants['nodes-id']})
 
     # add a column for each way count
-    intersections_df['nodes1'] = np.where(
-        intersections_df['edge_count'] == 1, 1, 0)
-    intersections_df['nodes3'] = np.where(
-        intersections_df['edge_count'] == 3, 1, 0)
-    intersections_df['nodes4'] = np.where(
-        intersections_df['edge_count'] > 3, 1, 0)
+    intersections_df['nodes1'] = np.where(intersections_df['edge_count'] == 1, 1, 0)
+    intersections_df['nodes2'] = np.where(intersections_df['edge_count'] == 2, 1, 0)
+    intersections_df['nodes3'] = np.where(intersections_df['edge_count'] == 3, 1, 0)
+    intersections_df['nodes4p'] = np.where(intersections_df['edge_count'] > 3, 1, 0)
 
     locals_d = {
         'network': network,
@@ -117,8 +102,8 @@ def buffer_parcels(settings, buffer_parcels_spec,
         'parcels_df': parcel_data_df,
         'intersections_df': intersections_df,
         'poi_df': poi_df,
-        'poi_x': constants['stops-x'],
-        'poi_y': constants['stops-y'],
+        'poi_x': constants['pois-x'],
+        'poi_y': constants['pois-y'],
         'parcel_index': constants['parcel_index']
     }
 
@@ -126,17 +111,12 @@ def buffer_parcels(settings, buffer_parcels_spec,
         locals_d.update(constants)
 
     results, trace_results, trace_assigned_locals \
-        = buffer.buffer_variables(
-            buffer_parcels_spec, 'parcels_df', locals_d,
-            trace_rows=trace_parcel_rows)
-
+        = buffer.buffer_variables(buffer_parcels_spec, 'parcels_df',
+                                  locals_d, trace_rows=trace_parcel_rows)
     results = results.fillna(0)
 
-    # since parcels_data_df is local in buffer.py,
-    # it can come back with extra columns:
-    del_cols = list(
-        set(parcel_data_df.columns) - set(
-            parcel_data.to_frame().columns))
+    # since parcels_data_df is local in buffer.py, it can come back with extra columns:
+    del_cols = list(set(parcel_data_df.columns) - set(parcel_data.to_frame().columns))
     parcel_data_df = parcel_data_df.drop(del_cols, axis=1)
 
     for column in results.columns:
@@ -145,13 +125,10 @@ def buffer_parcels(settings, buffer_parcels_spec,
     if trace_parcels:
 
         if trace_results is None:
-            logger.warn(
-                "trace_parcels not found in parcels = %s" % (
-                    trace_parcels))
+            logger.warn("trace_parcels not found in parcels = %s" % (trace_parcels))
         else:
             df = parcel_data_df.loc[parcel_data_df[trace_parcel_rows].index]
-            df = df.merge(
-                trace_results, how='left', left_index=True, right_index=True)
+            df = df.merge(trace_results, how='left', left_index=True, right_index=True)
 
             tracing.trace_df(df,
                              label='buffered_parcels',
@@ -163,10 +140,8 @@ def buffer_parcels(settings, buffer_parcels_spec,
                 tracing.write_locals(df, file_name="netbuffer_locals")
 
     # write result
-    buffered_parcels_fname = os.path.join(
-        output_dir, buffer_parcels_settings["parcel_output_file"])
-    pipeline.get_table(
-        "parcel_data").to_csv(buffered_parcels_fname, index=False)
+    buffered_parcels_fname = os.path.join(output_dir, buffer_parcels_settings["parcel_output_file"])
+    pipeline.get_table("parcel_data").to_csv(buffered_parcels_fname, index=False)
 
 
 @inject.injectable()
@@ -183,20 +158,16 @@ def create_network(settings, create_network_settings, data_dir):
 
     # create pandana network
     logger.info("Running create_network")
-    nodes = pd.read_csv(os.path.join(
-        data_dir, create_network_settings["nodes"]))
-    links = pd.read_csv(os.path.join(
-        data_dir, create_network_settings["links"]))
+    nodes = pd.read_csv(os.path.join(data_dir, create_network_settings["nodes"]))
+    links = pd.read_csv(os.path.join(data_dir, create_network_settings["links"]))
+
     nodes.index = nodes[create_network_settings["nodes-id"]]
 
-    network = pdna.Network(
-        nodes[create_network_settings["nodes-x"]],
-        nodes[create_network_settings["nodes-y"]],
-        links[create_network_settings["links-a"]],
-        links[create_network_settings["links-b"]],
-        links[[create_network_settings["links-impedance"]]],
-        twoway=create_network_settings["twoway"])
+    network = pdna.Network(nodes[create_network_settings["nodes-x"]],
+                           nodes[create_network_settings["nodes-y"]],
+                           links[create_network_settings["links-a"]],
+                           links[create_network_settings["links-b"]],
+                           links[[create_network_settings["links-impedance"]]],
+                           twoway=create_network_settings["twoway"])
 
-    network.save_hdf5(
-        os.path.join(data_dir, create_network_settings["output"]))
-
+    network.save_hdf5(os.path.join(data_dir, create_network_settings["output"]))

netbuffer/core/buffer.py
@@ -130,10 +130,10 @@ def buffer_variables(buffer_expressions,
     targets as the assigned target name.
 
     parcel_df_name is the name of the data frame in locals_dict, which represents
-    point data, to which all buffering and network distance measurements are 
-    applied. In order to do this, each row (point) in parcel_df must be associated with it's 
-    nearest node in the Pandana network. This is achieved using the Pandana method 
-    network.get_node_ids. The buffering operations are performed on each node in the 
+    point data, to which all buffering and network distance measurements are
+    applied. In order to do this, each row (point) in parcel_df must be associated with it's
+    nearest node in the Pandana network. This is achieved using the Pandana method
+    network.get_node_ids. The buffering operations are performed on each node in the
     network, thus allowing the results to be joined to the parcel df via node_id. Only
     the results that share the same nodes in the parcel_df_name data frame
     are returned.
@@ -195,10 +195,11 @@ def buffer_variables(buffer_expressions,
         if x is None or np.isscalar(x):
             if target:
                 logger.warn("WARNING: assign_variables promoting scalar %s to series" % target)
-            x = pd.Series([x] * len(locals_dict[parcel_df_name].index), index=locals_dict[parcel_df_name].index)
+            x = pd.Series([x] * len(locals_dict[parcel_df_name].index),
+                          index=locals_dict[parcel_df_name].index)
         if not isinstance(x, pd.Series):
             x = pd.Series(x)
-        x.name = target 
+        x.name = target
 
         return x
 
@@ -209,16 +210,16 @@ def buffer_variables(buffer_expressions,
         if trace_rows.any():
             trace_results = []
             trace_assigned_locals = {}
-    
+
     # avoid touching caller's passed-in locals_d parameter (they may be looping)
     locals_dict = locals_dict.copy() if locals_dict is not None else {}
     local_keys = locals_dict.keys()
-    
-    l = []
+
+    le = []
     traceable = True
     # need to be able to identify which variables causes an error, which keeps
     # this from being expressed more parsimoniously
-    for e in zip(buffer_expressions.target, buffer_expressions.variable, 
+    for e in zip(buffer_expressions.target, buffer_expressions.variable,
                  buffer_expressions.target_df, buffer_expressions.expression):
         target, var, target_df, expression = e
 
@@ -242,34 +243,38 @@ def buffer_variables(buffer_expressions,
             save_err = np.seterr(all='log')
 
             network = locals_dict['network']
-            
+            print("solve expression: " + expression)
+
             # aggregate query
             if 'aggregate' in expression:
-                network.set(locals_dict[target_df][locals_dict['node_id']], 
+                network.set(locals_dict[target_df][locals_dict['node_id']],
                             variable=locals_dict[target_df][var], name=var)
                 values = to_series(eval(expression, globals(), locals_dict), target=target)
                 # index results to the parcel_df:
-                locals_dict[parcel_df_name][target] = values.loc[locals_dict[parcel_df_name][locals_dict['node_id']]].values
+                locals_dict[parcel_df_name][target] = \
+                    values.loc[locals_dict[parcel_df_name][locals_dict['node_id']]].values
                 values = locals_dict[parcel_df_name][target]
-                traceable = True 
-                
+                traceable = True
+
             # nearest poi
             elif 'nearest_pois' in expression:
-                # records we want to run nearest poi on should have a value of 1. Ex- Could have a table of transit stops, 
-                # where each column is a type of transit stop, e.g. light rail, and a value of 1 in the light rail column
-                # means that that stop is a light rail stop. 
+                # records we want to run nearest poi on should have a value of 1.
+                # Ex- Could have a table of transit stops,
+                # where each column is a type of transit stop, e.g. light rail, and a
+                # value of 1 in the light rail column
+                # means that that stop is a light rail stop.
                 temp_df = locals_dict[target_df][(locals_dict[target_df][var] == 1)]
-                network.set_pois(var, temp_df[locals_dict['poi_x']], temp_df[locals_dict['poi_x']])
-                # poi queries return a df, no need to put through to_series function. 
+                network.set_pois(var, temp_df[locals_dict['poi_x']], temp_df[locals_dict['poi_y']])
+                # poi queries return a df, no need to put through to_series function.
                 values = eval(expression, globals(), locals_dict)
-
                 # index results to the parcel_df:
-                locals_dict[parcel_df_name][target] = values.loc[locals_dict[parcel_df_name][locals_dict['node_id']]].values
+                locals_dict[parcel_df_name][target] = \
+                    values.loc[locals_dict[parcel_df_name][locals_dict['node_id']]].values
                 values = locals_dict[parcel_df_name][target]
                 # if assignment is to a df that is not the parcel df, then cannot trace results
-                if target_df <> parcel_df_name:
+                if target_df != parcel_df_name:
                     traceable = False
-                
+
             # panda df assignment:
             else:
                 values = to_series(eval(expression, globals(), locals_dict), target=target)
@@ -280,9 +285,9 @@ def buffer_variables(buffer_expressions,
                 locals_dict[target_df] = locals_dict[target_df].merge(pd.DataFrame(
                     values), how='left', left_index=True, right_index=True)
                 # if assignment is to a df that is not the parcel df, then cannot trace results
-                if target_df <> parcel_df_name:
+                if target_df != parcel_df_name:
                     traceable = False
-                    
+
             np.seterr(**save_err)
             np.seterrcall(saved_handler)
 
@@ -295,15 +300,15 @@ def buffer_variables(buffer_expressions,
             # values = to_series(None, target=target)
             raise err
 
-        l.append((target, values))
+        le.append((target, values))
 
         if trace_results is not None:
             # some calcs are not included in the final df so may not have the
             # parcels that being traced. These should have a value of 'None' in
-            # spec under the 'variable' column. 
-            if traceable: 
+            # spec under the 'variable' column.
+            if traceable:
                 trace_results.append((target, values[trace_rows]))
-        
+
         # update locals to allows us to ref previously assigned targets
         locals_dict[target] = values
 
@@ -313,7 +318,7 @@ def buffer_variables(buffer_expressions,
     # the first time we see them so they end up in execution order
     variables = []
     seen = set()
-    for statement in reversed(l):
+    for statement in reversed(le):
         # statement is a tuple (<target_name>, <eval results in pandas.Series>)
         target_name = statement[0]
         if not is_temp(target_name) and target_name not in seen:
@@ -322,13 +327,11 @@ def buffer_variables(buffer_expressions,
 
     # DataFrame from list of tuples [<target_name>, <eval results>), ...]
     variables = pd.DataFrame.from_items(variables)
-
     if trace_results is not None:
         trace_results = pd.DataFrame.from_items(trace_results)
         trace_results.index = locals_dict[parcel_df_name][trace_rows].index
         trace_results = undupe_column_names(trace_results)
 
         # add df columns to trace_results
-        #trace_results = pd.concat([locals_dict[parcel_df_name], trace_results], axis=1)
-
+        # trace_results = pd.concat([locals_dict[parcel_df_name], trace_results], axis=1)
     return variables, trace_results, trace_assigned_locals

netbuffer/core/tests/test_buffer.py
@@ -1,15 +1,13 @@
 
 import os.path
-
 import numpy.testing as npt
 import numpy as np
 import pandas as pd
 import pandas.util.testing as pdt
 import pandana as pdna
 import pytest
-
-
 import orca
+
 
 from .. import buffer
 from activitysim.core import tracing
@@ -29,13 +27,16 @@ def configs_dir():
 def spec_name(data_dir):
     return os.path.join(data_dir, 'buffer_spec.csv')
 
+
 @pytest.fixture(scope='module')
 def net_name(data_dir):
     return os.path.join(data_dir, 'test_net.h5')
 
+
 @pytest.fixture(scope='module')
 def parcel_name(data_dir):
     return os.path.join(data_dir, 'test_parcels.csv')
+
 
 @pytest.fixture(scope='module')
 def data_name(data_dir):
@@ -51,7 +52,7 @@ def test_read_model_spec(spec_name):
 
     spec = buffer.read_buffer_spec(spec_name)
 
-    #assert len(spec) == 8
+    # assert len(spec) == 8
 
     assert list(spec.columns) == ['description', 'target', 'variable', 'target_df', 'expression']
 
@@ -59,32 +60,27 @@ def test_read_model_spec(spec_name):
 def test_buffer_variables(capsys, spec_name, net_name, parcel_name):
 
     spec = buffer.read_buffer_spec(spec_name)
-  
+
     network = pdna.Network.from_hdf5(net_name)
-
-    #network.init_pois(num_categories=constants["num_categories"], 
-                      #max_dist=constants["max_dist"], max_pois=constants["max_pois"])
-
     parcel_data_df = pd.read_csv(parcel_name)
     assert len(parcel_data_df) == 3
 
-    # 
     parcel_data_df['node_id'] = network.get_node_ids(parcel_data_df['xcoord_p'].values,
                                                      parcel_data_df['ycoord_p'].values)
 
     # parcel 1219983 should be assigned to network node 82030 becuase it is the nearest node:
-    assert int(parcel_data_df[parcel_data_df['parcelid']==735313].node_id) == 84076
-    print parcel_data_df[parcel_data_df['parcelid']==735313].node_id
+    assert int(parcel_data_df[parcel_data_df['parcelid'] == 735313].node_id) == 84076
+    print parcel_data_df[parcel_data_df['parcelid'] == 735313].node_id
 
     locals_d = {
         'network': network,
         'parcels_df': parcel_data_df,
         'node_id': 'node_id'
     }
-    #parcel_data_df.set_index(constants['parcel_index'], inplace=True)
-    #parcel_data_df.reset_index(level=0, inplace=True)
+    # parcel_data_df.set_index(constants['parcel_index'], inplace=True)
+    # parcel_data_df.reset_index(level=0, inplace=True)
 
-#    locals_d = {'CONSTANT': 7, '_shadow': 99}
+    # locals_d = {'CONSTANT': 7, '_shadow': 99}
 
     results, trace_results, trace_assigned_locals \
         = buffer.buffer_variables(spec, 'parcels_df', locals_d, trace_rows=None)
@@ -103,96 +99,25 @@ def test_buffer_variables(capsys, spec_name, net_name, parcel_name):
     results, trace_results, trace_assigned_locals \
         = buffer.buffer_variables(spec, 'parcels_df', locals_d, trace_rows=trace_parcel_rows)
 
-#    # should get same results as before
-    assert list(results.target3) == [383,383, 383]
+    # should get same results as before
+    assert list(results.target3) == [383, 383, 383]
 
-#    # should assign trace_results for second row in data
+    # should assign trace_results for second row in data
     print trace_results
 
-    #assert trace_results is not None
-    #assert '_temp' in trace_results.columns
-    #assert list(trace_results['_scalar']) == [42]
+    # assert trace_results is not None
+    # assert '_temp' in trace_results.columns
+    # assert list(trace_results['_scalar']) == [42]
 
-#    # shadow should have been assigned
-#    assert list(trace_results['_shadow']) == [1]
-#    assert list(trace_results['_temp']) == [9]
-#    assert list(trace_results['target3']) == [530]
+    # shadow should have been assigned
+    # assert list(trace_results['_shadow']) == [1]
+    # assert list(trace_results['_temp']) == [9]
+    # assert list(trace_results['target3']) == [530]
 
-#    print "trace_assigned_locals", trace_assigned_locals
-#    assert trace_assigned_locals['_DF_COL_NAME'] == 'thing2'
+    # print "trace_assigned_locals", trace_assigned_locals
+    # assert trace_assigned_locals['_DF_COL_NAME'] == 'thing2'
 
-#    # shouldn't have been changed even though it was a target
-#    assert locals_d['_shadow'] == 99
+    # shouldn't have been changed even though it was a target
+    # assert locals_d['_shadow'] == 99
 
     out, err = capsys.readouterr()
-
-
-#def test_assign_variables_aliased(capsys, data):
-
-#    spec_name = \
-#        os.path.join(os.path.dirname(__file__), 'data', 'assignment_spec_alias_df.csv')
-
-#    spec = assign.read_assignment_spec(spec_name)
-
-#    locals_d = {'CONSTANT': 7, '_shadow': 99}
-
-#    trace_rows = [False, True, False]
-
-#    results, trace_results, trace_assigned_locals \
-#        = assign.assign_variables(spec, data, locals_d,
-#                                  df_alias='aliased_df', trace_rows=trace_rows)
-
-#    print results
-
-#    assert list(results.columns) == ['target1', 'target2', 'target3']
-#    assert list(results.target1) == [True, False, False]
-#    assert list(results.target2) == [53, 53, 55]
-#    assert list(results.target3) == [530, 530, 550]
-
-#    # should assign trace_results for second row in data
-#    print trace_results
-
-#    assert trace_results is not None
-#    assert '_scalar' in trace_results.columns
-#    assert list(trace_results['_scalar']) == [42]
-
-#    # shadow should have been assigned
-#    assert list(trace_results['_shadow']) == [1]
-#    assert list(trace_results['_temp']) == [9]
-#    assert list(trace_results['target3']) == [530]
-
-#    assert locals_d['_shadow'] == 99
-
-#    out, err = capsys.readouterr()
-
-
-#def test_assign_variables_failing(capsys, data):
-
-#    output_dir = os.path.join(os.path.dirname(__file__), 'output')
-#    orca.add_injectable("output_dir", output_dir)
-
-#    tracing.config_logger(basic=True)
-
-#    spec_name = \
-#        os.path.join(os.path.dirname(__file__), 'data', 'assignment_spec_failing.csv')
-
-#    spec = assign.read_assignment_spec(spec_name)
-
-#    locals_d = {
-#        'CONSTANT': 7,
-#        '_shadow': 99,
-#        'log': np.log,
-#    }
-
-#    with pytest.raises(NameError) as excinfo:
-#        results, trace_results = assign.assign_variables(spec, data, locals_d, trace_rows=None)
-
-#    out, err = capsys.readouterr()
-#    # don't consume output
-#    print out
-
-#    # numpy warning should appear in log but not raise error
-#    assert 'invalid value encountered in log' in out
-
-#    # undefined variable should raise error
-#    assert "'undefined_variable' is not defined" in str(excinfo.value)